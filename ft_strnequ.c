01a28753ad376e702bdb978705c0dd5614a3fdbd
Deedzy42 <44777267+Deedzy42@users.noreply.github.com>
2018-11-28 17:27:53 +0100

    Add files via upload

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: amoachon <amoachon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/15 12:05:17 by amoachon          #+#    #+#             */
-/*   Updated: 2018/11/28 17:24:23 by amoachon         ###   ########.fr       */
+/*   Updated: 2018/11/28 17:27:28 by amoachon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,5 @@ int		ft_strnequ(char const *s1, char const *s2, size_t n)
 		s2++;
 		n--;
 	}
-	if (*s1 || *s2)
-		return (1);
-	return (0);
+	return (1);
 }
